23b2865a64c8e0654eef10a64f45e3ec2c6b0c86
Alice Harvey <66881641+har-v@users.noreply.github.com>
2020-06-19 21:48:48 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
-/*DATE AND TIME*/  
+/*DATE AND TIME*/
 var date = new Date();
+var day = date.getDate();
 var monthsArr = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'DEC'];
 var month = monthsArr[date.getMonth()];
 var year = date.getFullYear();
-var day = date.getDate();
-
 document.getElementById('day').textContent = day;
 document.getElementById('month').textContent = month;
 document.getElementById('year').textContent = year;
@@ -32,15 +31,24 @@ startTime();
 /*QUOTES--------------------------------------------------------------------------------------------------------------*/
 var quotes = [{japanese: "出る杭は打たれる", english: "The stake that sticks up gets hammered down."},
             {japanese: "知らぬが仏", english: "Not knowing is Buddha."},
-            {japanese: "十人十色", english: "Ten persons, ten colours."},
+            {japanese: "十人十色", english: "Ten persons, ten colors."},
             {japanese: "因果応報", english: "Cause bring result / bad causes bring bad results."},
             {japanese: "弱肉強食", english: "The weak are meat; the strong eat."},];
-
 
 function getQuote() {
     var randomQuote = Math.floor(Math.random() * quotes.length);
     document.getElementById('japanese').innerHTML = quotes[randomQuote].japanese;
     document.getElementById('english').innerHTML = quotes[randomQuote].english;
 }
-
 /*AUDIO----------------------------------------------------------------------------------------------------------------*/
+var audio = document.getElementById("audioPlayer");
+
+function getAudio() {
+    if (audio.paused) {
+        audio.play();
+        document.getElementById("audioBtn").style.backgroundImage="url(icons8-audio-24.png)";
+    } else {
+        audio.pause();
+        document.getElementById("audioBtn").style.backgroundImage="url(icons8-mute-24.png)";
+    }
+}
